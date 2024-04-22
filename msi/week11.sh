096efbe4c34c0209807c70e8882374b2daa756ee
Chulin Chen <chen6496@ln1002.mangi.msi.umn.edu>
2024-04-22 07:01:42 -0500

    add all files

---
+++ msi/week11.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=128
+#SBATCH --mem=16gb
+#SBATCH -t 01:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chen6496@umn.edu
+#SBATCH -p msismall
+cd ~/psy8712-week11/msi
+module load R/4.3.0-openblas
+Rscript week11-cluster.R
