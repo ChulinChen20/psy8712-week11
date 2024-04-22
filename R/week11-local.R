35e31cb5ef014a59cd1cbd2ebf02a60d969461c8
[chulinchen20] <[chen6496@umn.edu]>
2024-04-22 00:56:24 -0500

    update all scripts

---
+++ R/week11-local.R
@@ -0,0 +1,137 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(haven)
+library(caret)
+library(tictoc)
+library(parallel)
+library(doParallel)
+
+# Data Import and Cleaning
+gss <- read_sav("../data/GSS2016.sav")
+
+gss_tbl<- gss  %>%
+  # Remove the other two “work hours” variables
+  select(-HRS2, -HRS1)   %>%
+  filter(is.na(MOSTHRS) != T) %>%
+  rename(workhours = MOSTHRS) %>%
+  select(where(~mean(is.na(.)) < 0.75)) %>%
+  mutate(workhours = as.numeric(workhours)) 
+
+gss_tbl %>% 
+  ggplot(aes(workhours)) +
+  geom_freqpoly()
+
+# Analysis
+split <- sample(1:nrow(gss_tbl), .75*nrow(gss_tbl))
+
+gss_train <- gss_tbl[split, ]
+
+gss_test <- gss_tbl[-split, ]
+
+algo <- c("lm", "glmnet", "ranger", "xgbTree") 
+
+grids <- function(x) {
+  if(x == "lm") {
+    tuneGrid = NULL
+  } else if(x=="glmnet") {
+    tuneGrid = expand.grid(
+      alpha=0:1,
+      lambda=seq(0.0001,1,length=20)
+    )
+  } else if(x=="ranger"){
+    
+    tuneGrid = data.frame(
+      .mtry = c(2,3,7),
+      .splitrule = "variance",
+      .min.node.size = 5
+    )
+  } else  {
+    tuneGrid = expand.grid(
+      nrounds = 100,
+      eta = c(0.01, 0.001, 0.0001),
+      max_depth = c(2, 4, 6, 8),
+      gamma = 0, 
+      subsample = 1,
+      min_child_weight = c(1, 2, 3), 
+      colsample_bytree = 1
+    )
+  }
+}
+
+table1_tbl <- c()
+table2_tbl <- c()
+
+# fit models
+for(i in algo) {
+  set.seed(12)
+  
+  #original model
+  tic()
+  model1 <- train(
+    workhours~.,
+    gss_train,
+    method = i,
+    na.action = na.pass,
+    tuneGrid = grids(i),
+    preProcess = c("center","scale","zv","nzv","medianImpute"),
+    trControl=trainControl(method="cv",number=10, verboseIter=T)
+  )
+  t1<- toc()
+  
+  # turn on parallel processing
+  local_cluster <- makeCluster(7)
+  registerDoParallel(local_cluster)
+  
+  # parallelized models
+  tic()
+  model2 <- train(
+    workhours~.,
+    gss_train,
+    method = i,
+    na.action = na.pass,
+    tuneGrid = grids(i),
+    preProcess = c("center","scale","zv","nzv","medianImpute"),
+    trControl=trainControl(method="cv",number=10, verboseIter=T)
+  )
+  t2<- toc()
+
+  # turn off parallel processing
+  stopCluster(local_cluster)
+  registerDoSEQ()
+  
+  # Publication
+  cv_rsq <- max(model1$results$Rsquared)
+  ho_rsq <- cor(predict(model1, gss_test,na.action = na.pass),gss_test$workhours)^2
+  table <- cbind(i,str_remove(format(round(cv_rsq,2),nsmall=2),"^0"),str_remove(format(round(ho_rsq,2),nsmall=2),"^0"))
+  table1_tbl  <- rbind(table1_tbl ,table)
+  colnames(table1_tbl) <- c("algo","cv_rsq","ho_rsq")
+  
+  # combine algorithm names, time taken for original, time taken for parallelized into a table
+  original <- t1$toc - t1$tic
+  parallelized <- t2$toc - t2$tic
+  table2 <- cbind(i,original,parallelized)
+  table2_tbl <- rbind(table2_tbl,table2)
+  colnames(table2_tbl) <- c("algo","original","parallelized")
+}
+
+tibble(table1_tbl)
+tibble(table2_tbl)
+
+write_csv(data.frame(table1_tbl),"../out/table1.csv")
+write_csv(data.frame(table2_tbl),"../out/table2.csv")
+
+# Q1: xgboost. This is because the time taken for parallelization includes
+#time spent on assigning tasks to different cores and time spent on the 
+#tasks themselves, so that algorithms that take longer to run benefit more
+# from parallelization. XGBoost is an ensemble model that can run multiple trees
+# in parallel, which takes a lot of time if each task is run sequentially
+# as evident by the fact that it takes most time to run in the original model.
+# Therefore, it benefit the most when tasks can be run in parallel.
+# Q2: (xgbTree)134.194 - (glmnet)7.248 = 126.946.The difference is proportional to
+# the difference among different algorithms obtained by the original models, 
+# although the original difference between the two is much larger. The reason is 
+# that subtrees in xgbTree takes longer to run compared to individual glmnet models.
+# Q3: elastic net: because it provides among the best prediction on both training 
+# and test data. It also takes relatively less time to run compared to models 
+# with similar level of performance (i.e. XGBoost).
---
+++ msi/week11-cluster.R
@@ -0,0 +1,125 @@
+# Script Settings and Resources
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(haven)
+library(caret)
+library(tictoc)
+library(parallel)
+library(doParallel)
+
+# Data Import and Cleaning
+gss <- read_sav("../data/GSS2016.sav")
+
+gss_tbl<- gss  %>%
+  # Remove the other two “work hours” variables
+  select(-HRS2, -HRS1)   %>%
+  filter(is.na(MOSTHRS) != T) %>%
+  rename(workhours = MOSTHRS) %>%
+  select(where(~mean(is.na(.)) < 0.75)) %>%
+  mutate(workhours = as.numeric(workhours)) 
+
+# Analysis
+split <- sample(1:nrow(gss_tbl), .75*nrow(gss_tbl))
+
+gss_train <- gss_tbl[split, ]
+
+gss_test <- gss_tbl[-split, ]
+
+algo <- c("lm", "glmnet", "ranger", "xgbTree") 
+
+grids <- function(x) {
+  if(x == "lm") {
+    tuneGrid = NULL
+  } else if(x=="glmnet") {
+    tuneGrid = expand.grid(
+      alpha=0:1,
+      lambda=seq(0.0001,1,length=20)
+    )
+  } else if(x=="ranger"){
+    
+    tuneGrid = data.frame(
+      .mtry = c(2,3,7),
+      .splitrule = "variance",
+      .min.node.size = 5
+    )
+  } else  {
+    tuneGrid = expand.grid(
+      nrounds = 100,
+      eta = c(0.01, 0.001, 0.0001),
+      max_depth = c(2, 4, 6, 8),
+      gamma = 0, 
+      subsample = 1,
+      min_child_weight = c(1, 2, 3), 
+      colsample_bytree = 1
+    )
+  }
+}
+
+Table3 <- c()
+Table4 <- c()
+
+
+for(i in algo) {
+  set.seed(12)
+  
+
+  tic()
+  model1 <- train(
+    workhours~.,
+    gss_train,
+    method = i,
+    na.action = na.pass,
+    tuneGrid = grids(i),
+    preProcess = c("center","scale","zv","nzv","medianImpute"),
+    trControl=trainControl(method="cv",number=10, verboseIter=T)
+  )
+  t1<- toc()
+  
+
+  local_cluster <- makeCluster(7)
+  registerDoParallel(local_cluster)
+  
+
+  tic()
+  model2 <- train(
+    workhours~.,
+    gss_train,
+    method = i,
+    na.action = na.pass,
+    tuneGrid = grids(i),
+    preProcess = c("center","scale","zv","nzv","medianImpute"),
+    trControl=trainControl(method="cv",number=10, verboseIter=T)
+  )
+  t2<- toc()
+  
+
+  stopCluster(local_cluster)
+  registerDoSEQ()
+  
+  # Publication
+  cv_rsq <- max(model1$results$Rsquared)
+  ho_rsq <- cor(predict(model1, gss_test,na.action = na.pass),gss_test$workhours)^2
+  table <- cbind(i,str_remove(format(round(cv_rsq,2),nsmall=2),"^0"),str_remove(format(round(ho_rsq,2),nsmall=2),"^0"))
+  Table3  <- rbind(Table3,table)
+  colnames(Table3) <- c("algo","cv_rsq","ho_rsq")
+  
+  original <- t1$toc - t1$tic
+  parallelized <- t2$toc - t2$tic
+  table2 <- cbind(i,original,parallelized)
+  Table4 <- rbind(Table4,table2)
+  colnames(Table4) <- c("algo","supercomputer","supercomputer_128")
+}
+
+tibble(Table3)
+tibble(Table4)
+
+write_csv(data.frame(Table3),"../out/table3.csv")
+write_csv(data.frame(Table4),"../out/table4.csv")
+
+# Q1:similar to the locally run models, XGBoost benefit the most because of
+# the same reasons.
+# Q2: Assuming that the job is highly parallel, more cores means that more
+# tasks can be run at the same time, which leads to faster processing time.
+# Q3: I will still select elastic net based on a joined consideration of
+# performance and run time. I think the use of supercomputer is unnecessary 
+# for the model given that it runs pretty fast locally.
